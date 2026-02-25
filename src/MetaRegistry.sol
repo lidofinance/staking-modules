8e32235534bc7b37af677ea8eb79d028a4a11954
Dmitry Gusakov <gusakov.dv@gmail.com>
2026-02-25 11:04:31 +0100

    fix: STATE-1-02

--- src/MetaRegistry.sol
+++ src/MetaRegistry.sol
@@ -361,7 +361,7 @@ contract MetaRegistry is IMetaRegistry, Initializable, AccessControlEnumerableUp
     function _checkExternalOperatorExistsTypeNOR(ExternalOperator memory op) internal {
         (uint8 moduleId, uint64 noId) = op.unpackEntryTypeNOR();
         address module = _getOrCacheModuleAddress(moduleId);
-        _onlyExistingOperator(module, noId);
+        if (noId >= INodeOperatorsRegistry(module).getNodeOperatorsCount()) revert NodeOperatorDoesNotExist();
     }
 
     /// @dev Returns the module address for `moduleId`, resolving from
