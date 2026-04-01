d29e615739bb24bd579f2fa2d05361cbe06020ed
vgorkavenko <vladimir.g@lido.fi>
2026-04-01 15:46:49 +0200

    fix: parse nested contracts from tx

--- script/generateDiffyscanContracts.js
+++ script/generateDiffyscanContracts.js
@@ -8,21 +8,36 @@ function readJsonFile(path) {
 }
 
 function main() {
-  let result = {};
+  const result = {};
   const possibleArtifacts = fs.readdirSync(ARTIFACTS_DIR);
   if (!possibleArtifacts.includes(process.argv[2])) {
-    throw new Error(
-      "Invalid arg. Possible values: " + possibleArtifacts.join(", "),
-    );
+    throw new Error("Invalid arg. Possible values: " + possibleArtifacts.join(", "));
   }
   const transactions = readJsonFile(
     path.join(ARTIFACTS_DIR, process.argv[2], "transactions.json"),
   ).transactions;
-  transactions
-    .filter((tx) => tx.transactionType === "CREATE")
-    .forEach((tx) => {
+
+  transactions.forEach((tx) => {
+    if (tx.transactionType === "CREATE" && tx.contractAddress && tx.contractName) {
       result[tx.contractAddress] = tx.contractName;
-    });
+    }
+
+    let factoryContractName = null;
+    if (tx.function?.startsWith("create_gate_seal(")) {
+      factoryContractName = "GateSeal";
+    } else if (tx.contractName === "VettedGateFactory" && tx.function?.startsWith("create(")) {
+      factoryContractName = "OssifiableProxy";
+    }
+
+    if (factoryContractName) {
+      (tx.additionalContracts || []).forEach((nestedTx) => {
+        if (nestedTx.transactionType === "CREATE" && nestedTx.address) {
+          result[nestedTx.address] = factoryContractName;
+        }
+      });
+    }
+  });
+
   console.log(JSON.stringify(result, null, 2));
 }
 
