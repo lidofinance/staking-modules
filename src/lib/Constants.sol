db46c48d2b88810cd77773477ca834caceebff57
vgorkavenko <vladimir.g@lido.fi>
2026-06-09 15:00:52 +0200

    add todo

--- src/lib/Constants.sol
+++ src/lib/Constants.sol
@@ -2,5 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.33;
 
+// TODO: Think about the other constants to be placed here
+
 /// @dev Basis points denominator (100 % = 10 000 bp).
 uint256 constant MAX_BP = 10_000;
