59ddeaddf29248817fff08fdbb1e8270bd49dc00
madlabman <dmitry.c@lido.fi>
2026-07-22 13:27:09 +0000

    chore: update gindex.mjs to server gloas gindices

--- script/gindex.mjs
+++ script/gindex.mjs
@@ -2,19 +2,22 @@
 
 import { ssz } from "@lodestar/types";
 
-const FIELDS = {
-  gIWithdrawals: ["latestExecutionPayloadHeader", "withdrawalsRoot"],
-  gIValidators: ["validators"],
-  gIBalances: ["balances"],
-  gIHistoricalSummaries: ["historicalSummaries"],
-  gIBlockRoots: ["blockRoots"],
-};
+const FIELDS = [
+  ["gIWithdrawals", ["latestExecutionPayloadHeader", "withdrawalsRoot"]],
+  ["gIWithdrawals", ["payloadExpectedWithdrawals"]],
+  ["gIValidators", ["validators"]],
+  ["gIBalances", ["balances"]],
+  ["gIBlockRoots", ["blockRoots"]],
+  ["gIHistoricalSummaries", ["historicalSummaries"]],
+];
 
-for (const fork of ["electra"]) {
+for (const fork of ["electra", "gloas"]) {
   const Fork = ssz[fork];
-  for (const [name, path] of Object.entries(FIELDS)) {
-    const gI = Fork.BeaconState.getPathInfo(path).gindex;
-    console.log(`${fork}::${name}:`, `0x${gI.toString(16)}`);
+  for (const [name, path] of FIELDS) {
+    try {
+      const gI = Fork.BeaconState.getPathInfo(path).gindex;
+      console.log(`${fork}::${name}:`, `0x${gI.toString(16)}`);
+    } catch {}
   }
   console.log();
 }
