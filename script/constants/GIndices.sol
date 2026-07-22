933c705b719c306f28af64efcefdff113dfb62f8
madlabman <dmitry.c@lido.fi>
2026-07-22 13:26:50 +0000

    chore: change constants order

--- script/constants/GIndices.sol
+++ script/constants/GIndices.sol
@@ -8,13 +8,13 @@ import { GIndex } from "../../src/lib/GIndex.sol";
 library GIndices {
     GIndex public constant WITHDRAWALS_ELECTRA = GIndex.wrap(0xb0e);
     GIndex public constant VALIDATORS_ELECTRA = GIndex.wrap(0x4b);
-    GIndex public constant HISTORICAL_SUMMARIES_ELECTRA = GIndex.wrap(0x5b);
     GIndex public constant BALANCES_ELECTRA = GIndex.wrap(0x4c);
     GIndex public constant BLOCK_ROOTS_ELECTRA = GIndex.wrap(0x45);
+    GIndex public constant HISTORICAL_SUMMARIES_ELECTRA = GIndex.wrap(0x5b);
 
     GIndex public constant WITHDRAWALS_GLOAS = GIndex.wrap(0xb97);
     GIndex public constant VALIDATORS_GLOAS = GIndex.wrap(0x166);
-    GIndex public constant HISTORICAL_SUMMARIES_GLOAS = GIndex.wrap(0xb86);
     GIndex public constant BALANCES_GLOAS = GIndex.wrap(0x167);
     GIndex public constant BLOCK_ROOTS_GLOAS = GIndex.wrap(0x160);
+    GIndex public constant HISTORICAL_SUMMARIES_GLOAS = GIndex.wrap(0xb86);
 }
