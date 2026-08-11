b252587fca29b192b07039b69b5ca1589dfb2ab9
madlabman <dmitry.c@lido.fi>
2026-08-11 08:17:32 +0000

    chore: update csm 0x02 module type

--- script/csm0x02/DeployCSM0x02Hoodi.s.sol
+++ script/csm0x02/DeployCSM0x02Hoodi.s.sol
@@ -53,7 +53,7 @@ contract DeployCSM0x02Hoodi is DeployCSM0x02Base {
         config.setResetBondCurveAddress = 0x4AF43Ee34a6fcD1fEcA1e1F832124C763561dA53; // Dev team EOA
         config.chargePenaltyRecipient = 0x0534aA41907c9631fae990960bCC72d75fA7cfeD; // locator.treasury()
         // Module
-        config.moduleType = "community-onchain-v1"; // Just a unique type name to be used by the off-chain tooling
+        config.moduleType = "community-compounding-v1"; // Just a unique type name to be used by the off-chain tooling
         config.generalDelayedPenaltyReporter = 0x4AF43Ee34a6fcD1fEcA1e1F832124C763561dA53; // Dev team EOA
         // TODO: Reconsider the top-up queue limit value for CSM0x02.
         config.topUpQueueLimit = 32;
