cb3db2c17f29834bf4c9f3d8292801097f17a225
madlabman <dmitry.c@lido.fi>
2026-08-24 18:05:07 +0000

    refactor: add BLS enum member

--- src/utils/WithdrawalCredentials.sol
+++ src/utils/WithdrawalCredentials.sol
@@ -4,11 +4,11 @@
 pragma solidity 0.8.33;
 
 enum WCType {
+    BLS,
     Eth1,
     Compounding
 }
 
 function toWC(address withdrawalAddress, WCType wcType) pure returns (bytes32) {
-    bytes1 prefix = wcType == WCType.Eth1 ? bytes1(0x01) : bytes1(0x02);
-    return bytes32(prefix) | bytes32(uint256(uint160(withdrawalAddress)));
+    return bytes32(bytes1(uint8(wcType))) | bytes32(uint256(uint160(withdrawalAddress)));
 }
