b09228e7fda8d24e48432e4c688575bd21b38916
Sergey K <sergey.k@lido.fi>
2026-09-01 09:54:33 +0500

    fix: track node operator first deposit timestamp

--- test/unit/ModuleAbstract/NodeOperators.t.sol
+++ test/unit/ModuleAbstract/NodeOperators.t.sol
@@ -10,19 +10,6 @@ import { ValidatorBalanceLimits } from "src/lib/ValidatorBalanceLimits.sol";
 import { ModuleFixtures } from "./_Base.t.sol";
 
 abstract contract ModuleCreateNodeOperator is ModuleFixtures {
-    function test_getNodeOperatorCreatedAt_returnsZeroWhenNotRecorded() public view {
-        assertEq(module.getNodeOperatorCreatedAt(0), 0);
-    }
-
-    function test_createNodeOperator_setsCreatedAt() public {
-        uint256 createdAt = 1_754_000_000;
-        vm.warp(createdAt);
-
-        uint256 noId = createNodeOperator();
-
-        assertEq(module.getNodeOperatorCreatedAt(noId), createdAt);
-    }
-
     function test_createNodeOperator() public assertInvariants {
         uint256 nonce = module.getNonce();
         vm.expectEmit(address(module));
@@ -41,6 +28,14 @@ abstract contract ModuleCreateNodeOperator is ModuleFixtures {
         assertEq(module.getNonce(), nonce + 1);
         assertEq(nodeOperatorId, 0);
         assertEq(module.getNodeOperatorDepositInfoToUpdateCount(), 0);
+    }
+
+    function test_createNodeOperator_doesNotSetFirstDepositAt() public {
+        vm.warp(1_754_000_000);
+
+        uint256 noId = createNodeOperator();
+
+        assertEq(module.getNodeOperatorFirstDepositAt(noId), 0);
     }
 
     function test_createNodeOperator_whenDepositInfoUpdateIsRequested() public assertInvariants {
